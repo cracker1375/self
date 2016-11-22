52d05250904ef1facbd37711836f3cca5ea0c82f
cracker1375 <cracker1375@gmail.com>
2016-11-22 19:11:02 +0330

    Add files via upload

---
+++ plugins/gphelp.lua
@@ -0,0 +1,53 @@
+do
+    
+function run(msg, matches)
+  local help = [[Beyond Self Commands
+
+🔴دستورات مدیریتی گروه👇
+
+🔴!setname (name)
+🔹تغیر نام گروه به (name)
+
+🔴!link 
+🔹دریافت لینک گروه در پیوی
+
+🔴!newlink
+🔹ساخت لینک جدید
+
+🔴!setlink [link]
+🔹ثبت لینک گروه مورد نظر
+
+🔴!tosuper
+🔹تبدیل گروه معمولی به سوپر گوه
+
+🔴!setdes (text)
+🔹تغیر دسکریپشن گروه به (text)
+
+🔴!kick @username (by reply)
+🔹اخراج فردی با آیدی @username (حتی با ریپلای)
+
+🔴!inv @username (by reply)
+🔹ادد کردن فردی با آیدی @username به گروه (حتی با ریپلای)
+
+🔴!id @username (by reply)
+🔹دریافت آیدی عددی فردی با آیدی @username (حتی با ریپلای)
+
+🔴!gpid
+🔹دریافت آیدی گروه
+
+➖➖➖➖➖➖
+ID : 😎@B_Hat😎
+➖➖➖➖➖➖]]
+    if matches[1] == 'help' and is_sudo(msg) then
+      send_large_msg("user#id"..msg.from.id, help)      
+   return 'Help was sent in your private message'
+    end
+end 
+
+return {
+  patterns = {
+    "^[!/#](gphelp)$"
+  },
+  run = run
+}
+end
